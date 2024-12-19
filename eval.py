b27efbbfe57aae5bba37b1b7ba104a5c7fc78f8f
Khong Ngoc Anh <khongngocanh2004@gmail.com>
2024-12-20 02:22:48 +0700

    fix bug

--- eval.py
+++ eval.py
@@ -1,12 +1,14 @@
 from magent2.environments import battle_v4
-import os
-import cv2
 from torch_model import QNetwork
 from final_torch_model import QNetwork as FinalQNetwork
 from my_model import QMix
+import torch
 import numpy as np
 
-import torch
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = lambda x, *args, **kwargs: x  # Fallback: tqdm becomes a no-op
 
 def load_network(path, architecture, observation_space, action_space):
     if architecture == QMix:
@@ -19,110 +21,125 @@ def load_network(path, architecture, observation_space, action_space):
     )
     return model
 
-if __name__ == "__main__":
-    env = battle_v4.env(map_size=45, render_mode="rgb_array")
-    vid_dir = "video"
-    os.makedirs(vid_dir, exist_ok=True)
-    fps = 45
-    frames = []
+def eval():
+    max_cycles = 300
+    env = battle_v4.env(map_size=45, max_cycles=max_cycles)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     observation_space = env.observation_space("red_0").shape
     action_space = env.action_space("red_0").n
 
-    q_network_2 = load_network('blue.pt', FinalQNetwork, observation_space, action_space)
+    def random_policy(env, agent, obs):
+        return env.action_space(agent).sample()
 
-    qmix = load_network('qmix_ver1.pt', QMix, observation_space, action_space)
+    q_network = load_network('red.pt', QNetwork, observation_space, action_space)
+    q_network.to(device)
 
-    # random policies
-    env.reset()
-    for agent in env.agent_iter():
-        observation, reward, termination, truncation, info = env.last()
+    final_network = load_network('red_final.pt', FinalQNetwork, observation_space, action_space)
+    final_network.to(device)
 
-        if termination or truncation:
-            action = None  # this agent has died
-        else:
-            agent_handle = agent.split("_")[0]
-            if agent_handle == "blue":
-                observation = (
-                    torch.Tensor(observation).float().permute([2, 0, 1]).unsqueeze(0)
-                )
-                with torch.no_grad():
-                    q_values = q_network_2(observation)
-                    # q_values = qmix.get_q_values(observation)
-                action = torch.argmax(q_values, dim=1).numpy()[0]
-            else:
-                action = env.action_space(agent).sample()
+    my_network = load_network('blue.pt', FinalQNetwork, observation_space, action_space)
+    my_network.to(device)
 
 
-        env.step(action)
+    def pretrain_policy(env, agent, obs):
+        observation = (
+            torch.Tensor(obs).float().permute([2, 0, 1]).unsqueeze(0).to(device)
+        )
+        with torch.no_grad():
+            q_values = q_network(observation)
+        return torch.argmax(q_values, dim=1).cpu().numpy()[0]
+    
 
-        if agent == "red_0":
-            frames.append(env.render())
+    def final_pretrain_policy(env, agent, obs):
+        observation = (
+            torch.Tensor(obs).float().permute([2, 0, 1]).unsqueeze(0).to(device)
+        )
+        with torch.no_grad():
+            q_values = final_network(observation)
+        return torch.argmax(q_values, dim=1).cpu().numpy()[0]
 
-    height, width, _ = frames[0].shape
-    out = cv2.VideoWriter(
-        os.path.join(vid_dir, f"random.mp4"),
-        cv2.VideoWriter_fourcc(*"mp4v"),
-        fps,
-        (width, height),
+
+    def my_policy(env, agent, obs):
+        observation = (
+            torch.Tensor(obs).float().permute([2, 0, 1]).unsqueeze(0).to(device)
+        )
+        with torch.no_grad():
+            q_values = my_network(observation)
+        return torch.argmax(q_values, dim=1).cpu().numpy()[0]
+
+    def run_eval(env, red_policy, blue_policy, n_episode: int = 100):
+        red_win, blue_win = [], []
+        red_tot_rw, blue_tot_rw = [], []
+        n_agent_each_team = len(env.env.action_spaces) // 2
+
+        for _ in tqdm(range(n_episode)):
+            env.reset()
+            n_kill = {"red": 0, "blue": 0}
+            red_reward, blue_reward = 0, 0
+
+            for agent in env.agent_iter():
+                observation, reward, termination, truncation, info = env.last()
+                agent_team = agent.split("_")[0]
+
+                n_kill[agent_team] += (
+                    reward > 4.5
+                )  # This assumes default reward settups
+                if agent_team == "red":
+                    red_reward += reward
+                else:
+                    blue_reward += reward
+
+                if termination or truncation:
+                    action = None  # this agent has died
+                else:
+                    if agent_team == "red":
+                        action = red_policy(env, agent, observation)
+                    else:
+                        action = blue_policy(env, agent, observation)
+
+                env.step(action)
+
+            who_wins = "red" if n_kill["red"] >= n_kill["blue"] + 5 else "draw"
+            who_wins = "blue" if n_kill["red"] + 5 <= n_kill["blue"] else who_wins
+            red_win.append(who_wins == "red")
+            blue_win.append(who_wins == "blue")
+
+            red_tot_rw.append(red_reward / n_agent_each_team)
+            blue_tot_rw.append(blue_reward / n_agent_each_team)
+
+        return {
+            "winrate_red": np.mean(red_win),
+            "winrate_blue": np.mean(blue_win),
+            "average_rewards_red": np.mean(red_tot_rw),
+            "average_rewards_blue": np.mean(blue_tot_rw),
+        }
+
+    print("=" * 20)
+    print("Eval with random policy")
+    print(
+        run_eval(
+            env=env, red_policy=random_policy, blue_policy=my_policy, n_episode=30
+        )
     )
-    for frame in frames:
-        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        out.write(frame_bgr)
-    out.release()
-    print("Done recording random agents")
+    print("=" * 20)
 
-    # pretrained policies
-    frames = []
-    env.reset()
-
-
-    q_network = QNetwork(
-        env.observation_space("red_0").shape, env.action_space("red_0").n
+    print("Eval with trained policy")
+    print(
+        run_eval(
+            env=env, red_policy=pretrain_policy, blue_policy=my_policy, n_episode=30
+        )
     )
-    q_network.load_state_dict(
-        torch.load("red.pt", weights_only=True, map_location="cpu")
+    print("=" * 20)
+
+    print("Eval with final policy")
+    print(
+        run_eval(
+            env=env, red_policy=final_pretrain_policy, blue_policy=my_policy, n_episode=30
+        )
     )
-    for agent in env.agent_iter():
+    print("=" * 20)
 
-        observation, reward, termination, truncation, info = env.last()
 
-        if termination or truncation:
-            action = None  # this agent has died
-        else:
-            agent_handle = agent.split("_")[0]
-            if agent_handle == "red":
-                observation = (
-                    torch.Tensor(observation).float().permute([2, 0, 1]).unsqueeze(0)
-                )
-                with torch.no_grad():
-                    q_values = q_network(observation)
-                action = torch.argmax(q_values, dim=1).numpy()[0]
-            else:
-                observation = (
-                    torch.Tensor(observation).float().permute([2, 0, 1]).unsqueeze(0)
-                )
-                with torch.no_grad():
-                    q_values = q_network_2(observation)
-                    # q_values = qmix.get_q_values(observation)
-                action = torch.argmax(q_values, dim=1).numpy()[0]
-
-        env.step(action)
-
-        if agent == "red_0":
-            frames.append(env.render())
-
-    height, width, _ = frames[0].shape
-    out = cv2.VideoWriter(
-        os.path.join(vid_dir, f"pretrained.mp4"),
-        cv2.VideoWriter_fourcc(*"mp4v"),
-        fps,
-        (width, height),
-    )
-    for frame in frames:
-        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        out.write(frame_bgr)
-    out.release()
-    print("Done recording pretrained agents")
-
-    env.close()
+if __name__ == "__main__":
+    eval()
